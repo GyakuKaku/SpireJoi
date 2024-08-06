020f7229dc46cf4e18b1679cee1e3aff7fe00e63
gyaku <Gyaku_Kaku@163.com>
2024-08-07 00:07:41 +0800

    feat:powers

---
+++ src/main/java/org/joi/cards/power/Facade.java
@@ -0,0 +1,42 @@
+package org.joi.cards.power;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class Facade extends CustomCard {
+    public static final String ID = "SpireJoi:Facade";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/strike.png";
+    private static final int COST = 2;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.POWER;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public Facade() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = 5;
+        this.tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(3);
+        }
+
+    }
+}
