5615a3afe82d24bc43e2ce7e5723720c60e6f6b0
gyaku <Gyaku_Kaku@163.com>
2024-08-09 18:26:20 +0800

    feat:实现轴芯功能

--- src/main/java/org/joi/relics/ZhouXin.java
+++ src/main/java/org/joi/relics/ZhouXin.java
@@ -1,7 +1,11 @@
 package org.joi.relics;
 
 import basemod.abstracts.CustomRelic;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 public class ZhouXin extends CustomRelic {
@@ -18,12 +22,31 @@ public class ZhouXin extends CustomRelic {
         super(ID, ImageMaster.loadImage(IMG_PATH), RELIC_TIER, LANDING_SOUND);
     }
 
-    // 获取遗物描述，但原版游戏只在初始化和获取遗物时调用，故该方法等于初始描述
-    public String getUpdatedDescription() {
-        return this.DESCRIPTIONS[0];
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.owner != null &&
+                info.type != DamageInfo.DamageType.HP_LOSS &&
+                info.type != DamageInfo.DamageType.THORNS &&
+                damageAmount >= AbstractDungeon.player.currentHealth &&
+                damageAmount > 0
+        ) {
+            this.flash();
+            damageAmount = 0;
+
+            // 特殊无效处理
+            this.usedUp = true;
+            this.description = MSG[2];
+            this.tips.clear();
+            this.tips.add(new PowerTip(this.name, this.description));
+            this.initializeTips();
+            this.counter = -2;
+        }
+        return damageAmount;
     }
 
-    public AbstractRelic makeCopy() {
-        return new ZhouXin();
+    // 获取遗物描述，但原版游戏只在初始化和获取遗物时调用，故该方法等于初始描述
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
     }
 }
