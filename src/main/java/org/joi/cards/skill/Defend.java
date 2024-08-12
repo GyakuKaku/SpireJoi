cd0a0f6a45263b045381eca4a92780b0aabbd7a3
gyaku <Gyaku_Kaku@163.com>
2024-08-12 16:37:26 +0800

    feat:卡面更新

--- src/main/java/org/joi/cards/skill/Defend.java
+++ src/main/java/org/joi/cards/skill/Defend.java
@@ -14,7 +14,7 @@ public class Defend extends CustomCard {
     public static final String ID = "SpireJoi:Defend";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/strike.png";
+    private static final String IMG_PATH = "joi/img/cards/defend.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
