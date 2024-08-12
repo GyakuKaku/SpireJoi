4e710d0f27243e38cfa7f6ad6264d8f43d433e92
gyaku <Gyaku_Kaku@163.com>
2024-08-12 18:25:02 +0800

    feat:哭哭(卡牌)实现

---
+++ src/main/java/org/joi/cards/attack/Cry.java
@@ -0,0 +1,69 @@
+package org.joi.cards.attack;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.infos.SleepyDamageInfo;
+import org.joi.powers.SleepyPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class Cry extends CustomCard {
+    public static final String ID = "SpireJoi:Cry";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/cry.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+
+    public Cry() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = 6;
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(
+                new DamageAllEnemiesAction(
+                        p,
+                        this.multiDamage,
+                        this.damageTypeForTurn,
+                        AbstractGameAction.AttackEffect.BLUNT_LIGHT
+                )
+        );
+        this.addToBot(
+                new ApplyPowerAction(
+                        m,
+                        p,
+                        new SleepyPower(
+                                m,
+                                this.magicNumber
+                        ),
+                        this.magicNumber
+                )
+        );
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(3);
+        }
+    }
+}
