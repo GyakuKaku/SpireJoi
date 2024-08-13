dea24846ab37e583515f6073a2ffd0ee1aa19828
gyaku <Gyaku_Kaku@163.com>
2024-08-13 18:15:24 +0800

    feat:睡意(能力)调整

--- src/main/java/org/joi/powers/SlumberPower.java
+++ src/main/java/org/joi/powers/SlumberPower.java
@@ -1,6 +1,5 @@
 package org.joi.powers;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -14,9 +13,9 @@ import org.joi.SpireJoi;
 import org.joi.infos.SleepyDamageInfo;
 import org.joi.patches.CardTagEnum;
 
-public class SleepyPower extends AbstractPower {
+public class SlumberPower extends AbstractPower {
     // 能力的ID
-    public static final String POWER_ID = "SpireJoi:SleepyPower";
+    public static final String POWER_ID = "SpireJoi:SlumberPower";
     // 能力的本地化字段
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     // 能力的名称
@@ -24,27 +23,32 @@ public class SleepyPower extends AbstractPower {
 
     private static final String IMG_PATH = "joi/img/icons/facade.png";
 
-    public SleepyPower(AbstractCreature owner, int amount) {
+    public SlumberPower(AbstractCreature owner) {
         this.name = NAME;
         this.ID = POWER_ID;
         this.owner = owner;
         this.img = ImageMaster.loadImage(IMG_PATH);
         this.type = PowerType.DEBUFF;
-        this.amount = amount;
+        this.amount = -1;
         this.updateDescription();
     }
 
-    // 每回合减少一层
+    // 减少伤害
     @Override
-    public void atEndOfRound() {
-        if (this.amount == 0) {
-            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
-        } else {
-            addToBot(new ReducePowerAction(this.owner, this.owner, POWER_ID, 1));
+    public float atDamageGive(float damage, DamageInfo.DamageType type) {
+        if (type == DamageInfo.DamageType.NORMAL) {
+            return damage * 0.75F;
         }
+        return damage;
     }
 
-    // 计算被攻击伤害
+    // 每回合是取消
+    @Override
+    public void atEndOfRound() {
+        addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+    }
+
+    // 被攻击时
     @Override
     public float atDamageReceive(float damage, DamageInfo.DamageType damageType, AbstractCard card) {
         if (card.hasTag(CardTagEnum.SLEEPY)) {
@@ -52,25 +56,10 @@ public class SleepyPower extends AbstractPower {
             // 不做特殊处理
         } else {
             SpireJoi.logger.info("计算普通伤害");
-            // 增加伤害
+            // 增加伤害并结束睡意状态
             damage = damage + this.amount;
         }
         return damage;
-    }
-
-    // 叠到五层取消并进入困倦状态
-    @Override
-    public void stackPower(int stackAmount) {
-        this.fontScale = 8.0F;
-        this.amount += stackAmount;
-
-        if (this.amount >= 5) {
-            SpireJoi.logger.info("睡意叠加到五层");
-            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
-            this.addToBot(new ApplyPowerAction(this.owner, this.owner, new SlumberPower(this.owner)));
-        } else {
-            SpireJoi.logger.info("睡意叠加:" + this.amount);
-        }
     }
 
     // 被攻击时
@@ -81,15 +70,16 @@ public class SleepyPower extends AbstractPower {
             // 不做特殊处理
         } else {
             SpireJoi.logger.info("受到普通伤害");
-            // 结束睡意状态
-            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+            // 睡意状态
+            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+            // 到五层转为困倦状态
+
         }
         return damageAmount;
     }
-
     
     @Override
     public void updateDescription() {
-        this.description = powerStrings.DESCRIPTIONS[0] + this.amount + powerStrings.DESCRIPTIONS[1];
+        this.description = powerStrings.DESCRIPTIONS[0];
     }
 }
