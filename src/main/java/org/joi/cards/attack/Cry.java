5b92744d1f002a3dd7d6c63745e7e24e66cef02b
gyaku <Gyaku_Kaku@163.com>
2024-08-15 21:39:02 +0800

    fix:哭哭(卡牌)效果

--- src/main/java/org/joi/cards/attack/Cry.java
+++ src/main/java/org/joi/cards/attack/Cry.java
@@ -9,7 +9,7 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
 
@@ -48,7 +48,7 @@ public class Cry extends CustomCard {
                     new ApplyPowerAction(
                             mo,
                             p,
-                            new VulnerablePower(
+                            new WeakPower(
                                     mo,
                                     this.magicNumber,
                                     false
