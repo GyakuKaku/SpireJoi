43d35d70f129b99d4e1310cc1a2a5eda38b396aa
gyaku <Gyaku_Kaku@163.com>
2024-08-16 17:47:03 +0800

    feat:大猫(遗物)实现

--- src/main/java/org/joi/relics/Cat.java
+++ src/main/java/org/joi/relics/Cat.java
@@ -2,6 +2,10 @@ package org.joi.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -37,9 +41,14 @@ public class Cat extends CustomRelic {
         if (attackPlayerCheck > 1) {
             // 攻击敌人
             AbstractMonster target = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
-
+            if (target != null) {
+                this.addToBot(new RelicAboveCreatureAction(target, this));
+                this.addToBot(new DamageAction(target, new DamageInfo(AbstractDungeon.player, 5, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+            }
         } else {
             // 攻击玩家
+            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            this.addToBot(new DamageAction(AbstractDungeon.player, new DamageInfo(AbstractDungeon.player, 5, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
         }
     }
 
