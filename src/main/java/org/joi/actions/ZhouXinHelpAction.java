609dae3dab8497af3ee60e426e3b27c01f2925b9
gyaku <Gyaku_Kaku@163.com>
2024-08-18 00:29:45 +0800

    feat:轴芯的支援(卡牌)实现

---
+++ src/main/java/org/joi/actions/ZhouXinHelpAction.java
@@ -0,0 +1,52 @@
+package org.joi.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+public class ZhouXinHelpAction extends AbstractGameAction {
+    private boolean freeToPlayOnce;
+    private int damage;
+    private AbstractPlayer p;
+    private DamageInfo.DamageType damageTypeForTurn;
+    private int energyOnUse;
+
+    public ZhouXinHelpAction(AbstractPlayer p, AbstractMonster m, int damage, DamageInfo.DamageType damageTypeForTurn, boolean freeToPlayOnce, int energyOnUse) {
+        this.p = p;
+
+        this.damage = damage;
+        this.freeToPlayOnce = freeToPlayOnce;
+        this.duration = Settings.ACTION_DUR_XFAST;
+        this.actionType = AbstractGameAction.ActionType.SPECIAL;
+        this.damageTypeForTurn = damageTypeForTurn;
+        this.energyOnUse = energyOnUse;
+    }
+
+    public void update() {
+        int effect = EnergyPanel.totalCount;
+        if (this.energyOnUse != -1) {
+            effect = this.energyOnUse;
+        }
+
+        if (this.p.hasRelic("Chemical X")) {
+            effect += 2;
+            this.p.getRelic("Chemical X").flash();
+        }
+
+        if (effect > 0) {
+            for (int i = 0; i < effect; i++) {
+                AbstractMonster target = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+                addToBot(new DamageAction(target, new DamageInfo(this.p, this.damage, this.damageTypeForTurn), AttackEffect.BLUNT_LIGHT));
+            }
+            if (!this.freeToPlayOnce) {
+                this.p.energy.use(EnergyPanel.totalCount);
+            }
+        }
+        this.isDone = true;
+    }
+}
---
+++ src/main/java/org/joi/cards/attack/ZhouXinHelp.java
@@ -0,0 +1,43 @@
+package org.joi.cards.attack;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.ZhouXinHelpAction;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class ZhouXinHelp extends CustomCard {
+    public static final String ID = "SpireJoi:ZhouXinHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/strike.png";
+    private static final int COST = -1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public ZhouXinHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = 7;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ZhouXinHelpAction(p, m, this.damage, this.damageTypeForTurn, this.freeToPlayOnce, this.energyOnUse));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            // 将该卡牌的伤害提高3点。
+            this.upgradeDamage(3);
+        }
+    }
+}
