92f61351cf4e8748662283f51f65779d62784556
gyaku <Gyaku_Kaku@163.com>
2024-08-19 23:04:25 +0800

    feat:卡牌添加

---
+++ src/main/java/org/joi/cards/skill/OrganizeDesk.java
@@ -0,0 +1,44 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.unique.GamblingChipAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class OrganizeDesk extends CustomCard {
+    public static final String ID = "SpireJoi:OrganizeDesk";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/slack_off.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public OrganizeDesk() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!AbstractDungeon.player.hand.isEmpty()) {
+            addToBot(new GamblingChipAction(AbstractDungeon.player, true));
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            initializeDescription();
+        }
+    }
+}
