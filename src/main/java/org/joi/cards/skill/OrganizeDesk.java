0f680bcc15389f8b6611a27f754856ee0092af05
gyaku <Gyaku_Kaku@163.com>
2024-08-20 13:47:40 +0800

    feat:桌面整理(卡牌)实现

--- src/main/java/org/joi/cards/skill/OrganizeDesk.java
+++ src/main/java/org/joi/cards/skill/OrganizeDesk.java
@@ -14,7 +14,7 @@ public class OrganizeDesk extends CustomCard {
     public static final String ID = "SpireJoi:OrganizeDesk";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/slack_off.png";
+    private static final String IMG_PATH = "joi/img/cards/organize_desk.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
