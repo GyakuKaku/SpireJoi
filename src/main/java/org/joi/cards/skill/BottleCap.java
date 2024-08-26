69f3287b267648e1bf2869e9824be0ad800ee724
gyaku <Gyaku_Kaku@163.com>
2024-08-26 17:43:01 +0800

    feat:卡面更新

--- src/main/java/org/joi/cards/skill/BottleCap.java
+++ src/main/java/org/joi/cards/skill/BottleCap.java
@@ -16,7 +16,7 @@ public class BottleCap extends CustomCard {
     public static final String ID = "SpireJoi:BottleCap";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/slack_off.png";
+    private static final String IMG_PATH = "joi/img/cards/bottle_cap.png";
     private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
