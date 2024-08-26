1eefdaea2e58a80c5a3bd333c239072ecb939b80
gyaku <Gyaku_Kaku@163.com>
2024-08-26 23:09:49 +0800

    feat:有空就睡(能力)实现

--- src/main/java/org/joi/powers/SleepWhenFreePower.java
+++ src/main/java/org/joi/powers/SleepWhenFreePower.java
@@ -1,5 +1,8 @@
 package org.joi.powers;
 
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
@@ -14,6 +17,8 @@ public class SleepWhenFreePower extends AbstractPower {
     // 能力的名称
     private static final String NAME = powerStrings.NAME;
     private static final String IMG_PATH = "joi/img/icons/facade.png";
+    private boolean gainEnergyNext = false;
+    private boolean firstTurn = false;
 
     public SleepWhenFreePower(AbstractCreature owner, int amount) {
         this.name = NAME;
@@ -22,9 +27,28 @@ public class SleepWhenFreePower extends AbstractPower {
         this.img = ImageMaster.loadImage(IMG_PATH);
         this.type = PowerType.BUFF;
         this.amount = amount;
+        this.gainEnergyNext = false;
+        this.firstTurn = false;
         this.updateDescription();
     }
-    
+
+    @Override
+    public void atStartOfTurn() {
+        if (this.gainEnergyNext && !this.firstTurn) {
+            flash();
+            this.addToBot(new GainEnergyAction(this.amount));
+        }
+        this.firstTurn = false;
+        this.gainEnergyNext = true;
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.type == AbstractCard.CardType.ATTACK) {
+            this.gainEnergyNext = false;
+        }
+    }
+
     @Override
     public void updateDescription() {
         this.description = powerStrings.DESCRIPTIONS[0] + this.amount + powerStrings.DESCRIPTIONS[1];
