b3933238613818a60da49e5d2d06cc560f0643f1
gyaku <Gyaku_Kaku@163.com>
2024-08-26 14:51:27 +0800

    feat:能量图标更新

---
+++ src/main/java/org/joi/cards/attack/Zatsudan.java
@@ -0,0 +1,53 @@
+package org.joi.cards.attack;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.ApplySleepyAction;
+import org.joi.infos.SleepyDamageInfo;
+import org.joi.patches.CardTagEnum;
+import org.joi.powers.SleepyPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class Zatsudan extends CustomCard {
+    public static final String ID = "SpireJoi:Zatsudan";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/start_live.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public Zatsudan() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = 2;
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+        this.tags.add(CardTagEnum.SLEEPY);
+        this.tags.add(CardTagEnum.LIVE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new SleepyDamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        this.addToBot(new ApplySleepyAction(m, p, new SleepyPower(m, this.magicNumber), this.magicNumber));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(3);
+            initializeDescription();
+        }
+    }
+}
