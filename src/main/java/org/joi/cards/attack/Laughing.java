4d328a37d9fe65e637d38491d176c9493bcfec7f
gyaku <Gyaku_Kaku@163.com>
2024-09-15 14:04:10 +0800

    feat:嘲笑(卡牌)实现

--- src/main/java/org/joi/cards/attack/Laughing.java
+++ src/main/java/org/joi/cards/attack/Laughing.java
@@ -2,24 +2,22 @@ package org.joi.cards.attack;
 
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import org.joi.actions.ApplySleepyAction;
-import org.joi.patches.CardTagEnum;
-import org.joi.powers.SleepyPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
 
-public class ASMR extends CustomCard {
-    public static final String ID = "SpireJoi:ASMR";
+public class Laughing extends CustomCard {
+    public static final String ID = "SpireJoi:Laughing";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/asmr.png";
+    private static final String IMG_PATH = "joi/img/cards/laughing.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.ATTACK;
@@ -27,22 +25,18 @@ public class ASMR extends CustomCard {
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
 
-    public ASMR() {
+    public Laughing() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = 8;
         this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
-        this.tags.add(CardTagEnum.SLEEPY);
-        this.tags.add(CardTagEnum.LIVE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
-            if (!mo.isDeadOrEscaped()) {
-                this.addToBot(new ApplySleepyAction(mo, p, new SleepyPower(mo, this.magicNumber), this.magicNumber));
-            }
+        if (m != null && (m.intent == AbstractMonster.Intent.DEFEND || m.intent == AbstractMonster.Intent.DEFEND_DEBUFF || m.intent == AbstractMonster.Intent.DEFEND_BUFF)) {
+            this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, this.magicNumber, false), this.magicNumber));
         }
     }
 
@@ -50,7 +44,8 @@ public class ASMR extends CustomCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(4);
+            this.upgradeDamage(3);
+            this.upgradeMagicNumber(1);
             this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
             initializeDescription();
         }
