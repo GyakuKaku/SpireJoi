6ef240f80667db1ba08dfb66a210bd7bdbb8cc8c
gyaku <Gyaku_Kaku@163.com>
2024-09-16 22:53:19 +0800

    feat:手机电台(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/MobileLive.java
@@ -0,0 +1,55 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.ApplySleepyAction;
+import org.joi.patches.CardTagEnum;
+import org.joi.powers.SleepyPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class MobileLive extends CustomCard {
+    public static final String ID = "SpireJoi:MobileLive";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/mobile_live.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public MobileLive() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = 7;
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+        this.tags.add(CardTagEnum.SLEEPY);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+            if (!mo.isDeadOrEscaped()) {
+                this.addToBot(new ApplySleepyAction(mo, p, new SleepyPower(mo, magicNumber), magicNumber));
+            }
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(4);
+            initializeDescription();
+        }
+    }
+}
