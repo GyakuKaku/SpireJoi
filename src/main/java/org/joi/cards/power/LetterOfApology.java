36bd91e64fefccc527cac74f521b0e16a03f8519
gyaku <Gyaku_Kaku@163.com>
2024-09-19 13:48:56 +0800

    feat:吃到道歉信(卡牌)实现

---
+++ src/main/java/org/joi/cards/power/LetterOfApology.java
@@ -0,0 +1,48 @@
+package org.joi.cards.power;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.powers.LetterOfApologyPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class LetterOfApology extends CustomCard {
+    public static final String ID = "SpireJoi:LetterOfApology";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/letter_of_apology.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.POWER;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public LetterOfApology() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = this.magicNumber = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new LetterOfApologyPower(p)));
+        if (this.upgraded) {
+            this.addToBot(new MakeTempCardInHandAction(new RingFitAdventure().makeCopy(), true));
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
