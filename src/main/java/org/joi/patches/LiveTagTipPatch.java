af6e7f4881b3a9175fa91f56124e6c7eb88d074f
gyaku <Gyaku_Kaku@163.com>
2024-09-20 21:42:18 +0800

    feat:直播牌tag说明(补丁)实现

--- src/main/java/org/joi/patches/LiveTagTipPatch.java
+++ src/main/java/org/joi/patches/LiveTagTipPatch.java
@@ -1,19 +1,24 @@
 package org.joi.patches;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.helpers.TipHelper;
 import javassist.CtBehavior;
-import org.joi.SpireJoi;
 
 import java.util.ArrayList;
 
+/**
+ * 为直播牌添加说明
+ */
 @SpirePatch(clz = TipHelper.class, method = "render")
 public class LiveTagTipPatch {
     @SpireInsertPatch(locator = Locator.class)
-    public static void Insert(TipHelper __instance) {
-        SpireJoi.logger.info("测试");
+    public static void Insert(SpriteBatch sb, AbstractCard ___card, ArrayList<String> ___KEYWORDS) {
+        if (___card != null && ___card.hasTag(CardTagEnum.LIVE) && !___KEYWORDS.contains("直播牌")) {
+            ___KEYWORDS.add("直播牌");
+        }
     }
-
     private static class Locator extends SpireInsertLocator {
         private Locator() {}
         public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
