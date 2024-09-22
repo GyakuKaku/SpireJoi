d6496924160729f7991eb9d764eb41c34e3c21ce
gyaku <Gyaku_Kaku@163.com>
2024-09-22 17:57:22 +0800

    feat:薯片(卡牌)实现

--- src/main/java/org/joi/cards/skill/PotatoChips.java
+++ src/main/java/org/joi/cards/skill/PotatoChips.java
@@ -1,22 +1,19 @@
 package org.joi.cards.skill;
 
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DexterityPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-import org.joi.powers.LoseBottleCapPower;
 
 import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
 
-public class BottleCap extends CustomCard {
-    public static final String ID = "SpireJoi:BottleCap";
+public class PotatoChips extends CustomCard {
+    public static final String ID = "SpireJoi:PotatoChips";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/bottle_cap.png";
+    private static final String IMG_PATH = "joi/img/cards/potato_chips.png";
     private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
@@ -24,17 +21,16 @@ public class BottleCap extends CustomCard {
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
 
-    public BottleCap() {
+    public PotatoChips() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
         this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new LoseBottleCapPower(p, this.magicNumber), this.magicNumber));
+        this.addToBot(new GainEnergyAction(this.magicNumber));
     }
 
     @Override
@@ -42,6 +38,7 @@ public class BottleCap extends CustomCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeMagicNumber(1);
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
