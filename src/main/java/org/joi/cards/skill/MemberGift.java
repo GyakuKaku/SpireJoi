25014094642578bd1f5390695539a94016c01c20
gyaku <Gyaku_Kaku@163.com>
2024-09-24 13:36:39 +0800

    feat:舰长礼物准备(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/MemberGift.java
@@ -0,0 +1,55 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.patches.CardTagEnum;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class MemberGift extends CustomCard {
+    public static final String ID = "SpireJoi:MemberGift";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/member_gift.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public MemberGift() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = 7;
+        this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber;
+        this.selfRetain = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+    }
+
+    @Override
+    public void onPlayCard(AbstractCard c, AbstractMonster m) {
+        if (c.hasTag(CardTagEnum.LIVE)) {
+            this.upgradeBlock(this.magicNumber);
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
