b27c12fbbab7467dae963f000cba936bea03109d
gyaku <Gyaku_Kaku@163.com>
2024-09-25 14:27:46 +0800

    feat:厕所歌(卡牌)实现

--- src/main/java/org/joi/cards/attack/WashroomSong.java
+++ src/main/java/org/joi/cards/attack/WashroomSong.java
@@ -2,47 +2,39 @@ package org.joi.cards.attack;
 
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
-import org.joi.effects.ZhouXinGoEffect;
-import org.joi.relics.ZhouXin;
+import org.joi.actions.ScareDamageAllEnemiesAction;
+import org.joi.patches.CardTagEnum;
 
 import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
 
-
-public class ZhouXinGo extends CustomCard {
-    public static final String ID = "SpireJoi:ZhouXinGo";
+public class WashroomSong extends CustomCard {
+    public static final String ID = "SpireJoi:WashroomSong";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/zhouxin_go.png";
-    private static final int COST = 2;
+    private static final String IMG_PATH = "joi/img/cards/washroom_song.png";
+    private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardColor COLOR = JOI_YELLOW;
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
 
-    public ZhouXinGo() {
+    public WashroomSong() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseDamage = 30;
+        this.baseDamage = 8;
         this.isMultiDamage = true;
+        this.tags.add(CardTagEnum.SCARE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractRelic zhouxin = p.relics.stream().filter(relic -> "SpireJoi:ZhouXin".equals(relic.relicId)).findFirst().orElse(null);
-        if (zhouxin != null && zhouxin instanceof ZhouXin) {
-            ((ZhouXin) zhouxin).invalidZhouXin(false);
-        }
-        this.addToBot(new VFXAction(new ZhouXinGoEffect(AbstractDungeon.player.hb.cX + AbstractDungeon.player.hb.width / 2.0F, AbstractDungeon.player.hb.cY), 0.0F));
-        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        this.addToBot(new ScareDamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
     }
 
     @Override
@@ -51,15 +43,28 @@ public class ZhouXinGo extends CustomCard {
         if (!canUse) {
             return false;
         }
-        AbstractRelic zhouxin = p.relics.stream().filter(relic -> "SpireJoi:ZhouXin".equals(relic.relicId)).findFirst().orElse(null);
-        return zhouxin != null && !zhouxin.usedUp;
+        return AbstractDungeon.actionManager.cardsPlayedThisTurn.stream().anyMatch(card -> card.hasTag(CardTagEnum.LIVE));
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.stream().noneMatch(card -> card.hasTag(CardTagEnum.LIVE))) {
+            return;
+        }
+        for (AbstractMonster m : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+            if (!m.isDeadOrEscaped() && m.hasPower("SpireJoi:SlumberPower")) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(6);
+            this.upgradeDamage(3);
             initializeDescription();
         }
     }
