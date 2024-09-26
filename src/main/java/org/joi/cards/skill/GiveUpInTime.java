5a731cee2575224c3c0ac42d37cead508a547ad7
gyaku <Gyaku_Kaku@163.com>
2024-09-26 16:16:15 +0800

    feat:及时放弃(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/GiveUpInTime.java
@@ -0,0 +1,48 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.ApplySleepyAction;
+import org.joi.powers.SleepyPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class GiveUpInTime extends CustomCard {
+    public static final String ID = "SpireJoi:GiveUpInTime";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/give_up_in_time.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public GiveUpInTime() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = 12;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+        this.addToBot(new ApplySleepyAction(p, p, new SleepyPower(p, 1), 1));
+        this.addToBot(new PressEndTurnButtonAction());
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(4);
+            initializeDescription();
+        }
+    }
+}
