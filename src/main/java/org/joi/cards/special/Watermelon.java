38e69a69f79db9ccae51c1ad3c65f2a70771aac6
gyaku <Gyaku_Kaku@163.com>
2024-09-27 17:52:11 +0800

    feat:西瓜(卡牌)实现

--- src/main/java/org/joi/cards/special/Watermelon.java
+++ src/main/java/org/joi/cards/special/Watermelon.java
@@ -1,30 +1,30 @@
-package org.joi.cards.skill;
+package org.joi.cards.special;
 
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
-
-public class SleepTalking extends CustomCard {
-    public static final String ID = "SpireJoi:SleepTalking";
+public class Watermelon extends CustomCard {
+    public static final String ID = "SpireJoi:Watermelon";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "joi/img/cards/sleep_talking.png";
+    private static final String IMG_PATH = "joi/img/cards/watermelon.png";
     private static final int COST = -2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
-    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardColor COLOR = AbstractCard.CardColor.COLORLESS;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
 
-    public SleepTalking() {
+    public Watermelon() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseMagicNumber = 4;
+        this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
     }
 
@@ -39,16 +39,16 @@ public class SleepTalking extends CustomCard {
 
     @Override
     public void triggerWhenDrawn() {
-        if (AbstractDungeon.player.hasPower("SpireJoi:SleepyPower") || AbstractDungeon.player.hasPower("SpireJoi:SlumberPower")) {
-            this.addToTop(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, this.magicNumber));
-        }
+        this.addToTop(new GainEnergyAction(this.magicNumber));
+        this.addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(3);
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            this.upgradeMagicNumber(1);
             initializeDescription();
         }
     }
