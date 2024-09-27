6953b0bd17e3f62f8dd7d100e85ca0f2d9819dc2
gyaku <Gyaku_Kaku@163.com>
2024-09-28 00:16:58 +0800

    feat:推理社社员的推理(卡牌)实现

---
+++ src/main/java/org/joi/actions/ReasoningAction.java
@@ -0,0 +1,55 @@
+package org.joi.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+public class ReasoningAction extends AbstractGameAction {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("BetterToHandAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+
+    public ReasoningAction(int amount) {
+        this.amount = amount;
+        this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    public void update() {
+        if (AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            this.isDone = true;
+            return;
+        }
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            if (AbstractDungeon.player.drawPile.isEmpty()) {
+                this.isDone = true;
+                return;
+            }
+            CardGroup tmpGroup = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+            if (this.amount != -1) {
+                for (int i = 0; i < Math.min(this.amount, AbstractDungeon.player.drawPile.size()); i++) {
+                    tmpGroup.addToTop(AbstractDungeon.player.drawPile.group.get(AbstractDungeon.player.drawPile.size() - i - 1));
+                }
+            } else {
+                for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
+                    tmpGroup.addToBottom(c);
+                }
+            }
+            AbstractDungeon.gridSelectScreen.open(tmpGroup, 1, false, TEXT[0]);
+        } else if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+            for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
+                if (AbstractDungeon.player.hand.size() == 10) {
+                    AbstractDungeon.player.createHandIsFullDialog();
+                    continue;
+                }
+                AbstractDungeon.player.drawPile.moveToHand(c, AbstractDungeon.player.drawPile);
+                c.triggerWhenDrawn();
+            }
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
+        }
+        this.tickDuration();
+    }
+}
---
+++ src/main/java/org/joi/cards/skill/NagisaHelp.java
@@ -0,0 +1,45 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.ReasoningAction;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class NagisaHelp extends CustomCard {
+    public static final String ID = "SpireJoi:NagisaHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/nagisa_help.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public NagisaHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+        this.baseMagicNumber = 3;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ReasoningAction(this.magicNumber));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBaseCost(1);
+            initializeDescription();
+        }
+    }
+}
