c7e0afcacc0ebcbaf447f9fc354c06227a3c9582
gyaku <Gyaku_Kaku@163.com>
2024-09-28 15:52:02 +0800

    feat:舰长礼物(卡牌)效果调整

--- src/main/java/org/joi/cards/skill/MemberGift.java
+++ src/main/java/org/joi/cards/skill/MemberGift.java
@@ -17,7 +17,7 @@ public class MemberGift extends CustomCard {
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String IMG_PATH = "joi/img/cards/member_gift.png";
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = JOI_YELLOW;
@@ -27,7 +27,7 @@ public class MemberGift extends CustomCard {
     public MemberGift() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseBlock = 7;
-        this.baseMagicNumber = 2;
+        this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
         this.selfRetain = true;
     }
