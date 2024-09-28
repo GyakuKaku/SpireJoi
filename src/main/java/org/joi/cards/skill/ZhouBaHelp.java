dfa4d9c7baf0193967255ac5504a027f6592c11c
gyaku <Gyaku_Kaku@163.com>
2024-09-28 21:19:42 +0800

    feat:轴爸的任务(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/ZhouBaHelp.java
@@ -0,0 +1,52 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import org.joi.cards.special.Watermelon;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class ZhouBaHelp extends CustomCard {
+    public static final String ID = "SpireJoi:ZhouBaHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/organize_desk.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public ZhouBaHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, 1), 1));
+        Watermelon watermelon = new Watermelon();
+        if (this.upgraded) {
+            watermelon.upgrade();
+        }
+        this.addToBot(new MakeTempCardInHandAction(watermelon, 1));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
