95b1e5d46e4190d338650f212d0dc0396df73756
gyaku <Gyaku_Kaku@163.com>
2024-09-28 22:30:40 +0800

    feat:招财猫的祝福(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/KitiHelp.java
@@ -0,0 +1,44 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class KitiHelp extends CustomCard {
+    public static final String ID = "SpireJoi:KitiHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/kiti_help.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public KitiHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 13;
+        this.magicNumber = this.baseMagicNumber;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.getCurrRoom().addGoldToRewards(this.magicNumber);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBaseCost(1);
+            initializeDescription();
+        }
+    }
+}
