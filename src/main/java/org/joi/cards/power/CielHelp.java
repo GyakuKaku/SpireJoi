9c447a81baa9a5c2b881d1976f724ba195162356
gyaku <Gyaku_Kaku@163.com>
2024-09-29 13:53:36 +0800

    feat:社畜天使的支援(卡牌)实现

---
+++ src/main/java/org/joi/cards/power/CielHelp.java
@@ -0,0 +1,45 @@
+package org.joi.cards.power;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.powers.CielHelpPower;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class CielHelp extends CustomCard {
+    public static final String ID = "SpireJoi:CielHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/ciel_help.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.POWER;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public CielHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = this.magicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int stack = this.magicNumber;
+        this.addToBot(new ApplyPowerAction(p, p, new CielHelpPower(p, stack), stack));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
