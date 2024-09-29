3d80518901983dfc2c1d208e961cb87499167be1
gyaku <Gyaku_Kaku@163.com>
2024-09-29 17:18:32 +0800

    feat:招财猫的保护(卡牌)实现

--- src/main/java/org/joi/cards/skill/KitiHelp.java
+++ src/main/java/org/joi/cards/skill/KitiHelp.java
@@ -1,11 +1,13 @@
 package org.joi.cards.skill;
 
 import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.powers.KitiHelpPower;
 
 import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
 
@@ -14,7 +16,7 @@ public class KitiHelp extends CustomCard {
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String IMG_PATH = "joi/img/cards/kiti_help.png";
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = JOI_YELLOW;
@@ -23,14 +25,15 @@ public class KitiHelp extends CustomCard {
 
     public KitiHelp() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseMagicNumber = 13;
+        this.baseMagicNumber = 3;
         this.magicNumber = this.baseMagicNumber;
         this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.getCurrRoom().addGoldToRewards(this.magicNumber);
+        AbstractDungeon.getCurrRoom().addGoldToRewards(13);
+        this.addToBot(new ApplyPowerAction(p, p, new KitiHelpPower(p, this.magicNumber), this.magicNumber));
     }
 
     @Override
---
+++ src/main/java/org/joi/powers/KitiHelpPower.java
@@ -0,0 +1,38 @@
+package org.joi.powers;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class KitiHelpPower extends AbstractPower {
+    // 能力的ID
+    public static final String POWER_ID = "SpireJoi:KitiHelpPower";
+    // 能力的本地化字段
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    // 能力的名称
+    private static final String NAME = powerStrings.NAME;
+    private static final String IMG_PATH = "joi/img/icons/kiti_help.png";
+
+    public KitiHelpPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.img = ImageMaster.loadImage(IMG_PATH);
+        this.type = PowerType.BUFF;
+        this.amount = amount;
+        this.updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        this.addToBot(new GainBlockAction(this.owner, this.owner, this.amount));
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = powerStrings.DESCRIPTIONS[0] + this.amount + powerStrings.DESCRIPTIONS[1];
+    }
+}
