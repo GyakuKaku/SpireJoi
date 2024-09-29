9258fc84d3a84842b90cc2846cd71794e46bd297
gyaku <Gyaku_Kaku@163.com>
2024-09-29 22:42:47 +0800

    feat:外星小丑的惊喜(卡牌)实现

--- src/main/java/org/joi/actions/TocciHelpAction.java
+++ src/main/java/org/joi/actions/TocciHelpAction.java
@@ -1,9 +1,10 @@
 package org.joi.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.*;
+import org.joi.powers.AfterTalkPower;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,25 +13,42 @@ import java.util.Random;
 
 public class TocciHelpAction extends AbstractGameAction {
 
-    public TocciHelpAction(AbstractCreature target, int amount) {
+    public TocciHelpAction(int amount) {
         this.amount = amount;
         this.actionType = AbstractGameAction.ActionType.POWER;
     }
 
     @Override
     public void update() {
-
-
+        for (int i = 0; i < this.amount; i++) {
+            this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, this.getRandomPowerId()));
+        }
+        this.isDone = true;
     }
 
     /**
      * 获取随机能力
      * @return
      */
-    private AbstractPower getRandomPower() {
+    private AbstractPower getRandomPowerId() {
         List<AbstractPower> powerList = new ArrayList<>();
-
-
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new StrengthPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new DexterityPower(AbstractDungeon.player, 1));
+        powerList.add(new AfterTalkPower(AbstractDungeon.player, 1));
+        powerList.add(new JuggernautPower(AbstractDungeon.player, 1));
+        powerList.add(new BufferPower(AbstractDungeon.player, 1));
+        powerList.add(new ThornsPower(AbstractDungeon.player, 2));
+        powerList.add(new PlatedArmorPower(AbstractDungeon.player, 2));
         Collections.shuffle(powerList, new Random(AbstractDungeon.shuffleRng.randomLong()));
         return powerList.get(0);
     }
---
+++ src/main/java/org/joi/cards/skill/TocciHelp.java
@@ -0,0 +1,44 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.actions.TocciHelpAction;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class TocciHelp extends CustomCard {
+    public static final String ID = "SpireJoi:TocciHelp";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/tocci_help.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public TocciHelp() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new TocciHelpAction(this.magicNumber));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
