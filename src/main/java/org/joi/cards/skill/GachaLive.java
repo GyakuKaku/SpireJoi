96ae2a4b451d37e8291ec8b81139069750602d93
gyaku <Gyaku_Kaku@163.com>
2024-09-30 17:35:33 +0800

    feat:画头像(卡牌)实现

---
+++ src/main/java/org/joi/cards/skill/GachaLive.java
@@ -0,0 +1,57 @@
+package org.joi.cards.skill;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.joi.cards.special.BadCard;
+import org.joi.cards.special.GoodCard;
+import org.joi.patches.CardTagEnum;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+public class GachaLive extends CustomCard {
+    public static final String ID = "SpireJoi:GachaLive";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/gacha_live.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public GachaLive() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+        this.baseBlock = 6;
+        this.tags.add(CardTagEnum.LIVE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+        CustomCard card = AbstractDungeon.cardRandomRng.randomBoolean(0.3F) ? new GoodCard() : new BadCard();
+        this.addToBot(new MakeTempCardInHandAction(card, 1));
+        if (this.upgraded) {
+            card = AbstractDungeon.cardRandomRng.randomBoolean(0.3F) ? new GoodCard() : new BadCard();
+            this.addToBot(new MakeTempCardInHandAction(card, 1));
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(3);
+            this.upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
