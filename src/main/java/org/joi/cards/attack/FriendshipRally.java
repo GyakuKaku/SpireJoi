0f66e405469e12b6a1839d31d37fd15c09340da0
gyaku <Gyaku_Kaku@163.com>
2024-10-02 19:38:49 +0800

    feat:效果图标调整

---
+++ src/main/java/org/joi/cards/attack/FriendshipRally.java
@@ -0,0 +1,57 @@
+package org.joi.cards.attack;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static org.joi.patches.PlayerColorEnum.JOI_YELLOW;
+
+
+public class FriendshipRally extends CustomCard {
+    public static final String ID = "SpireJoi:FriendshipRally";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "joi/img/cards/friendship_rally.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = JOI_YELLOW;
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public FriendshipRally() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = 18;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (long i = 0; i < p.powers.stream().map(power -> power.ID.endsWith("HelpPower")).count(); i++) {
+            this.addToBot(new DamageAction(m, new DamageInfo( p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        boolean canUse = super.canUse(p, m);
+        if (!canUse) {
+            return false;
+        }
+        return p.powers.stream().map(power -> power.ID.endsWith("HelpPower")).count() >= 3;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(6);
+            initializeDescription();
+        }
+    }
+}
